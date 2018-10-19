62daf32d020c95be392a4e7676ff08f2d9af54ae
Sumeet Agrawal <sumeetawl06@gmail.com>
2018-10-19 11:04:20 +0530

    complete

--- Pods/TOCropViewController/Objective-C/TOCropViewController/TOCropViewController.h
+++ Pods/TOCropViewController/Objective-C/TOCropViewController/TOCropViewController.h
@@ -85,7 +85,7 @@
 
 /**
  The minimum croping aspect ratio. If set, user is prevented from setting cropping rectangle to lower aspect ratio than defined by the parameter.
- */
+ */ 
 @property (nonatomic, assign) CGFloat minimumAspectRatio;
 
 /**
